28f921462f596964b71b93341b1ae1676ba0f61f
ErdalOzcelik297 <139227815+ErdalOzcelik297@users.noreply.github.com>
2023-08-09 16:31:43 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,14 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+        <Text variant="h1">TravelBuddy</Text>
         <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
+          This service has been created as a final project for the Customer Engagement and Artificial Intelligence (H9CEAI) course to provide travel package recommendations for those interested in visiting Turkey.
         </Text>
       </section>
 
       <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+        <Text variant="h2">AI-powered Digital Assistant: Makes it easy for you</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>
